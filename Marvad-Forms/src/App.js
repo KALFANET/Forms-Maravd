8e77a7a98b239083e0f21ea92f9f9b85f1552357
KALFANET <104522435+KALFANET@users.noreply.github.com>
2024-11-29 19:33:47 +0200

    Update App.js

--- Marvad-Forms/src/App.js
+++ Marvad-Forms/src/App.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { AuthProvider } from './context/AuthContext';
 import ClerkPortal from './components/clerk/ClerkPortal';
@@ -6,14 +5,15 @@ import CitizenForm from './components/citizen/CitizenForm';
 import { useAuth } from './hooks/useAuth';
 
 const App = () => {
-    const { isClerk } = useAuth();
-    return (
-        <AuthProvider>
-            <div className="min-h-screen bg-gray-50 py-8">
-                {isClerk ? <ClerkPortal /> : <CitizenForm />}
-            </div>
-        </AuthProvider>
-    );
+  const { isClerk } = useAuth();
+
+  return (
+    <AuthProvider>
+      <div className="min-h-screen bg-gray-50 py-8">
+        {isClerk ? <ClerkPortal /> : <CitizenForm />}
+      </div>
+    </AuthProvider>
+  );
 };
 
 export default App;
